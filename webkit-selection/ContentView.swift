3bddd0ea319e6b17fde48510b501a80ef7f4531e
phongle <phongle6893@gmail.com>
2026-05-22 17:42:51 +0700

    Initial Commit

---
+++ webkit-selection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  webkit-selection
+//
+//  Created by Phong Le on 22/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ webkit-selection/webkit_selectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  webkit_selectionApp.swift
+//  webkit-selection
+//
+//  Created by Phong Le on 22/5/26.
+//
+
+import SwiftUI
+
+@main
+struct webkit_selectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
